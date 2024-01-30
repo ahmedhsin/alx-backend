c2c341aa0973f7fe3ac2a13d54d8bf9157452cff
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-01-30 02:59:46 +0000

    3 task

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,1 +1,64 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> dict:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        ds = len(self.dataset())
+        ids = self.indexed_dataset()
+        if not index:
+            index = 0
+        assert index >= 0
+        assert index <= ds - 1
+        items = []
+        last = None
+        for key, val in ids.items():
+            if index >= key:
+                items.append(val)
+            if len(items) == index + page_size:
+                last = key + 1
+                break
+
+        dic = {
+                'index': index,
+                'next_index': last,
+                'page_size': len(items),
+                'data': items
+                }
+        return dic
