1bca6165f5faad90440782aa0b0f8d5f13a01e62
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-07 04:33:30 +0200

    add task 4, 5

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""1. Basic Babel setup
+"""
+
+from flask import Flask, render_template, request
+
+from flask_babel import Babel
+
+
+class Config():
+    """config for the flask app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get local"""
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello_world() -> str:
+    """this is a route"""
+    return render_template('4-index.html')
